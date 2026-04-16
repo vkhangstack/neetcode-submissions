1910eed00a1a8b98cab9a4f519680d1b1c992a33
Pham Van Khang <phamvankhang.tvi@gmail.com>
2026-04-17 00:33:31 +0700

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.go
@@ -0,0 +1,10 @@
+func twoSum(numbers []int, target int) []int {
+    for i := 0; i < len(numbers); i++ {
+        for j := i + 1; j < len(numbers); j++ {
+            if numbers[i]+numbers[j] == target {
+                return []int{i + 1, j+1}
+            }
+        }
+    }
+    return []int{}
+}
